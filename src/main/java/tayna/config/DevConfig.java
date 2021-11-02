d564542784dbae20ffe6a178ca17f868ff8e2fce
taynacosta <taynamoreira1811@gmail.com>
2021-11-02 11:22:55 -0300

    criando perfil de desenvolvedor, conectando com o mysql usando docker

---
+++ src/main/java/tayna/config/DevConfig.java
@@ -0,0 +1,32 @@
+package tayna.config;
+
+import org.apache.tomcat.util.json.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import tayna.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException, java.text.ParseException {
+
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
